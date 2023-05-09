85730b6e583a0ddba08e0499c9b443704cc55482
ShchepetkovDY <114948795+ShchepetkovDY@users.noreply.github.com>
2023-05-09 22:17:56 +0300

    Homework29

---
+++ users/urls/location.py
@@ -0,0 +1,7 @@
+from rest_framework import routers
+
+from users.views import LocationViewSet
+
+router = routers.SimpleRouter()
+router.register('location', LocationViewSet)
+urlpatterns = router.urls
